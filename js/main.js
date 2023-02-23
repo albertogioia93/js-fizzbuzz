4cc2483bd3598cc5cd948d71a5d948d292a45436
albertogioia93 <albertogioia@hotmail.it>
2023-02-23 17:23:06 +0100

    aggiunta commenti

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
+// creo una variabile per l'elemento ul list del mio html
 const ul = document.querySelector("ul.list");
 
-
+// uso il ciclo for per fare 100 numeri senza dover riscrivere ogni volta console.log per ogno numero
 for (let i = 1; i < 101; i++){
+    // creo una variabile dove ci assegno un elemento html creato qui ma visibile anche sulla pagina index.html
     const li = document.createElement('li'); 
     ul.append(li);
     
