7ee4bde29d121b1f3dba84c7c873908a250ca1ce
albertogioia93 <albertogioia@hotmail.it>
2023-02-23 17:00:30 +0100

    creazione del container nel DOM con append

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 'use strict';
 
-for (let i = 0; i < 101; i++){
+const ul = document.querySelector("ul.list");
+
+
+for (let i = 1; i < 101; i++){
+    const li = document.createElement('li'); 
+    ul.append(li);
+    
     if (i % 15 == 0){
         console.log("FizzBuzz");
+        li.append('FizzBuzz');
+        
     } else if (i % 3 == 0){
         console.log("Fizz");
+        li.append('Fizz');
     } else if (i % 5 == 0){
         console.log("Buzz");
+        li.append('Buzz');
     } else {
         console.log(i);
+        li.append(i);
     }
+
+
+    // const element = `<li class="box box--${i}">${i}</li>`;
+    // ul.innerHTML += element;
 }
