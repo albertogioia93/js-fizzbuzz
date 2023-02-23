f14326ffb9591867ae770f1734539b324769c5c3
albertogioia93 <albertogioia@hotmail.it>
2023-02-23 15:34:48 +0100

    prova numero 2

--- js/main.js
+++ js/main.js
@@ -1,8 +1,13 @@
 'use strict';
 
 for (let i = 0; i < 101; i++){
-    if (i % 15 == 0) console.log("FizzBuzz");
-    else if (i % 3 == 0) console.log("Fizz");
-    else if (i % 5 == 0) console.log("Buzz");
-    else console.log(i);
+    if (i % 15 == 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 == 0){
+        console.log("Fizz");
+    } else if (i % 5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
 }
